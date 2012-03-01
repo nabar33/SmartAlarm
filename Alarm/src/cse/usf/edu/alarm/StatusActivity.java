d049b413521a8cd6b704dabb2f735b7a281bad6a
Nathan Baron <Nathan@richthofen-macbook.local>
2012-02-29 20:28:14 -0500

    some bullcrap

--- Alarm/src/cse/usf/edu/alarm/StatusActivity.java
+++ Alarm/src/cse/usf/edu/alarm/StatusActivity.java
@@ -1,11 +1,10 @@
 package cse.usf.edu.alarm;
 
 import android.app.Activity;
-import android.app.TabActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class StatusActivity extends TabActivity {
+public class StatusActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
