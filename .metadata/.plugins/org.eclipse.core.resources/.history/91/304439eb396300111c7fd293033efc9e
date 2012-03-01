5e275adf986a963d6abcd3c156a710e227baa064
unknown <laptop@laptop-PC.(none)>
2012-02-29 20:00:36 -0500

    Changed alarms/places/status activities to TabActivities, added basic textviews to each

--- .metadata/.plugins/org.eclipse.core.resources/.history/91/304439eb396300111c7fd293033efc9e
+++ .metadata/.plugins/org.eclipse.core.resources/.history/91/304439eb396300111c7fd293033efc9e
@@ -1,11 +1,10 @@
 package cse.usf.edu.alarm;
 
 import android.app.Activity;
-import android.app.TabActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AlarmsActivity extends TabActivity {
+public class AlarmsActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
--- Alarm/src/cse/usf/edu/alarm/PlacesActivity.java
+++ Alarm/src/cse/usf/edu/alarm/PlacesActivity.java
@@ -1,13 +1,19 @@
 package cse.usf.edu.alarm;
 
 import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class PlacesActivity extends Activity {
+public class PlacesActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        TextView textview = new TextView(this);
+        textview.setText("This is the Places tab!");
+       
+        setContentView(textview);
     }
 }
