8c5e1dc6720566295be915dd02b232edbec64ca7
Nathan Baron <Nathan@richthofen-macbook.local>
2012-02-29 20:21:43 -0500

    Added TabHost code to SmartAlarmActivity

--- Alarm/src/cse/usf/edu/alarm/SmartAlarmActivity.java
+++ Alarm/src/cse/usf/edu/alarm/SmartAlarmActivity.java
@@ -1,7 +1,10 @@
 package cse.usf.edu.alarm;
 
 import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class SmartAlarmActivity extends TabActivity {
     /** Called when the activity is first created. */
@@ -9,5 +12,36 @@ public class SmartAlarmActivity extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Resources res = getResources();
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+        Intent intent;  // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, StatusActivity.class);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("status").setIndicator("Status",
+                          res.getDrawable(R.drawable.ic_tab_artists))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+        intent = new Intent().setClass(this, AlarmsActivity.class);
+        spec = tabHost.newTabSpec("alarms").setIndicator("Alarms",
+                          res.getDrawable(R.drawable.ic_tab_albums))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, PlacesActivity.class);
+        spec = tabHost.newTabSpec("places").setIndicator("Places",
+                          res.getDrawable(R.drawable.ic_tab_songs))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(2);
+    }
+        
     }
 }
