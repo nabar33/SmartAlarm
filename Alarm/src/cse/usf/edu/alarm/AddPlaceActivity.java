77fe128c5ef47b5a69c61d49f01a0e9efdda4a32
Nathan Baron <Nathan@richthofen-macbook.local>
2012-03-05 12:21:18 -0500

    Added AddAlarmActivity and AddPlaceActivity

---
+++ Alarm/src/cse/usf/edu/alarm/AddPlaceActivity.java
@@ -0,0 +1,12 @@
+package cse.usf.edu.alarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddPlaceActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
