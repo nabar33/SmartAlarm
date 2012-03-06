d2298fdd6a095bde2a4c760985cc3fee482216c2
Nathan Baron <Nathan@richthofen-macbook.local>
2012-03-05 23:21:19 -0500

    Added ListView to AlarmsActivity and populated it with weekdays.

--- Alarm/src/cse/usf/edu/alarm/AlarmsActivity.java
+++ Alarm/src/cse/usf/edu/alarm/AlarmsActivity.java
@@ -2,14 +2,22 @@ package cse.usf.edu.alarm;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class AlarmsActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.alarms_tab_view);
+        
+        ListView lview = (ListView) findViewById(R.id.weekdays);
+        String[] week_days = new String[] {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+        									R.layout.row_entry, R.id.day, week_days);
+        lview.setAdapter(adapter);
     }
 }
