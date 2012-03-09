ec587ceeccec0977024b72479eb8650a40e7918b
Jordi Lucero <jordi.lucero2@gmail.com>
2012-03-09 07:48:33 -0500

    Added Google Map on places tab

--- Alarm/src/cse/usf/edu/alarm/PlacesActivity.java
+++ Alarm/src/cse/usf/edu/alarm/PlacesActivity.java
@@ -1,11 +1,13 @@
 package cse.usf.edu.alarm;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class PlacesActivity extends Activity {
+public class PlacesActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,9 @@ public class PlacesActivity extends Activity {
     {
     	Intent intent = new Intent(this, AddPlaceActivity.class);
     	startActivity(intent);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
