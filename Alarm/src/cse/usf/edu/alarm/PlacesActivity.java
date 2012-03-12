51963fc88367aa507a6505df9776acf80a9a09cf
Jordi Lucero <jordi.lucero2@gmail.com>
2012-03-11 21:45:58 -0400

    Map Now go Zoom Zoom

--- Alarm/src/cse/usf/edu/alarm/PlacesActivity.java
+++ Alarm/src/cse/usf/edu/alarm/PlacesActivity.java
@@ -1,20 +1,30 @@
 package cse.usf.edu.alarm;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView.LayoutParams;  
+import android.view.View;
+import android.widget.LinearLayout;
 import com.google.android.maps.MapView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class PlacesActivity extends MapActivity {
-    /** Called when the activity is first created. */
+	MapView mapView; 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-       
+ 
         setContentView(R.layout.places_tab_view);
+        mapView = (MapView) findViewById(R.id.mapView);
+        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);  
+        View zoomView = mapView.getZoomControls(); 
+ 
+        zoomLayout.addView(zoomView, 
+            new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, 
+                LayoutParams.WRAP_CONTENT)); 
+        mapView.displayZoomControls(true);
     }
     
     public void startAddPlaceActivity(View view)
