f0568fce1f6356a7fb5e7356c2a5d7234d91477e
Clownwig <laptop@laptop-PC.(none)>
2012-04-03 16:30:47 -0400

    created service for GPS updates and for checking for alarm sounding time

--- Alarm/src/cse/usf/edu/alarm/SmartAlarmActivity.java
+++ Alarm/src/cse/usf/edu/alarm/SmartAlarmActivity.java
@@ -24,26 +24,7 @@ import android.widget.TabHost;
 import android.widget.Toast;
 
 public class SmartAlarmActivity extends TabActivity {
-	private final static int GPS_UPDATE_INTERVAL = 1000 * 60 * 10;
-	private final static int GPS_UPDATE_DISTANCE = 100;
-	LocationManager locationManager = (LocationManager) this
-			.getSystemService(Context.LOCATION_SERVICE);
-
-	LocationListener locationListener = new LocationListener() {
-		public void onLocationChanged(Location location) {
-			// Called when a new location is found by the network location
-			// provider.
-			try {
-				updateAlarmTimes(location);
-			} catch (Exception e) {
-				Toast.makeText(null, "Oh Lawd..", Toast.LENGTH_LONG);
-			}
-		}
-
-		public void onStatusChanged(String provider, int status, Bundle extras) {}
-		public void onProviderEnabled(String provider) {}
-		public void onProviderDisabled(String provider) {}
-	};
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -78,68 +59,8 @@ public class SmartAlarmActivity extends TabActivity {
 		tabHost.addTab(spec);
 
 		tabHost.setCurrentTab(0);
+		
+		startService(new Intent(this, AlarmService.class));
 
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, GPS_UPDATE_INTERVAL, GPS_UPDATE_DISTANCE, locationListener);
-	}
-	
-	public void updateAlarmTimes(Location location) throws Exception
-	{
-		Double latitude = location.getLatitude();
-		Double longitude = location.getLongitude();
-
-		StringBuilder urlString = new StringBuilder();
-		
-		String street = "blagh";
-		String city = "blah";
-		String state = "bah";
-		
-		urlString.append("https://maps.googleapis.com/maps/api/distancematrix/json?");
-		urlString.append("origins=");// from
-		urlString.append(Double.toString(latitude));
-		urlString.append(",");
-		urlString.append(Double.toString(longitude));
-		urlString.append("&destinations=");// to
-		street.replace(' ', '+');
-		city.replace(' ', '+');
-		state.replace(' ', '+');
-		urlString.append(street);
-		urlString.append("+");
-		urlString.append(city);
-		urlString.append("+");
-		urlString.append(state);
-		
-		urlString.append("&sensor=true");
-		
-		HttpURLConnection urlConnection = null;
-		URL url = new URL(urlString.toString());
-		urlConnection=(HttpURLConnection)url.openConnection();
-		urlConnection.setRequestMethod("GET");
-		urlConnection.setDoOutput(true);
-		urlConnection.setDoInput(true);
-		urlConnection.connect();
-		
-		InputStream inStream = urlConnection.getInputStream();
-		BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
-		
-		String temp, response = "";
-	    while((temp = bReader.readLine()) != null){
-	        //Parse data
-	        response += temp;
-	    }
-	    //Close the reader, stream & connection
-	    bReader.close();
-	    inStream.close();
-	    urlConnection.disconnect();
-		
-	    //Sortout JSONresponse 
-	    JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-	    JSONArray rows = object.getJSONArray("rows");
-
-	    JSONArray elements = rows.getJSONArray(0);
-
-	    JSONObject duration = elements.getJSONObject(1);
-	    
-	    int travelTime = duration.getInt("value");
-		
-	}
+		}
 }
