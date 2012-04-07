03bfc6229a434a113277077e713cce97d688dae0
Jordi Lucero <jordi.lucero2@gmail.com>
2012-04-07 05:18:28 -0400

    Made a Dumb Alarm Database and tried to implement it - needs more work

--- Alarm/src/cse/usf/edu/alarm/AlarmsActivity.java
+++ Alarm/src/cse/usf/edu/alarm/AlarmsActivity.java
@@ -29,6 +29,7 @@ import android.widget.Toast;
 public class AlarmsActivity extends Activity implements AdapterView.OnItemSelectedListener {
     
 	public AlarmDBManager myAlarmDB;
+	public DumbDBManager myDumbDB;
 	public static final String[] WEEK_DAYS = 
 		{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 	
@@ -42,11 +43,17 @@ public class AlarmsActivity extends Activity implements AdapterView.OnItemSelect
         myAlarmDB = new AlarmDBManager(this);
         myAlarmDB.open();
         
+        //Open Database for standard alarms
+        myDumbDB = new DumbDBManager(this);
+        myDumbDB.open();
+        
         // Populate Alarm spinner
         Spinner spinner = (Spinner) findViewById(R.id.day_spinner);
         spinner.setOnItemSelectedListener(this);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row_entry, R.id.day, WEEK_DAYS);
         spinner.setAdapter(adapter);
+        
+        
         
         // Fill remaining fields
         populateAlarmFields(WEEK_DAYS[0]);
@@ -66,6 +73,7 @@ public class AlarmsActivity extends Activity implements AdapterView.OnItemSelect
 				if(!(smart.getId()==checkedId)){
 					findViewById(R.id.bottom_smart_layout).setVisibility(View.VISIBLE);
 					findViewById(R.id.bottom_standard_layout).setVisibility(View.INVISIBLE);
+					populateDumbAlarm(WEEK_DAYS[0]);
 				}else{
 					findViewById(R.id.bottom_smart_layout).setVisibility(View.INVISIBLE);
 					findViewById(R.id.bottom_standard_layout).setVisibility(View.VISIBLE);
@@ -75,6 +83,44 @@ public class AlarmsActivity extends Activity implements AdapterView.OnItemSelect
 		});
 	}
 
+    
+	public void populateDumbAlarm(String selectedDay)
+    {
+    	int atime;
+    	try
+    	{
+    		Cursor alarmData = myDumbDB.getAlarm(selectedDay);
+    		
+    		if (alarmData.getColumnCount() > 2)
+    		{
+    			// Set Time Picker
+    			TimePicker timeSetter = (TimePicker) findViewById( R.id.place_time_timepicker );
+    			atime = alarmData.getInt(2);
+    			timeSetter.setCurrentHour(atime / 100);
+    			timeSetter.setCurrentMinute(atime % 100);
+
+    		}
+    		else
+    		{
+    			clearAlarmFields();
+    		}
+    		alarmData.close();
+    	}
+    	catch (SQLException squeak)
+    	{
+    		clearAlarmFields();
+    	}
+    	catch (CursorIndexOutOfBoundsException cioobe)
+    	{
+    		clearAlarmFields();
+    	}
+    	catch (IllegalStateException ise)
+    	{ // database is most likely not open
+    		myDumbDB.open();
+    		populateDumbAlarm(selectedDay); // this is dangerous
+    	}
+    }
+    
 
 	public void populateAlarmFields(String selectedDay)
     {
@@ -128,12 +174,81 @@ public class AlarmsActivity extends Activity implements AdapterView.OnItemSelect
     	timeSetter.setCurrentMinute(0);
     	
     	EditText ted = (EditText) findViewById(R.id.hour_edittext);
-		ted.setText("Hour");
 		ted = (EditText) findViewById(R.id.minute_edittext);
-		ted.setText("Minute");
 		
 		CheckBox checky = (CheckBox) findViewById(R.id.alarm_everyday_checkbox);
 		checky.setChecked(false);
+    }
+    
+    public void addDumbAlarm(View v)
+    {
+    	
+    	Spinner spinny = (Spinner) findViewById(R.id.day_spinner);
+    	int curr_day = spinny.getLastVisiblePosition();
+    	
+    	// Retrieve info from TimePicker
+    	TimePicker timeSetter = (TimePicker) findViewById(R.id.place_time_timepicker);
+    	int ahour = timeSetter.getCurrentHour();
+    	int aminute = timeSetter.getCurrentMinute();    	
+    	
+    	// Raw data
+    	int raw_alarm = ahour * 100 + aminute;
+
+    	/* Determine if Apply to All box is checked */
+    	CheckBox checky = (CheckBox) findViewById(R.id.alarm_everyday_checkbox);
+    	if (checky.isChecked()) // set alarm for all days
+    	{
+    		myDumbDB.deleteAllAlarms();
+    		for (String day : WEEK_DAYS)
+    		{
+    			myDumbDB.createAlarm(day, raw_alarm);
+    		}
+    		
+    		Context context = getApplicationContext();
+    		Toast.makeText(context, "Alarm Set for All Days", Toast.LENGTH_SHORT).show();
+    	}
+    	else  // set alarm for the current day only
+    	{
+    		/* Determine if an alarm already exists on that day */
+    		long id;
+    		try 
+    		{
+    			Cursor alarmData = this.myDumbDB.getAlarm(WEEK_DAYS[curr_day]);
+    			if (alarmData.getColumnCount() > 0)
+    				id = alarmData.getLong(0);
+    			else
+    				id = -1;
+    			alarmData.close();
+    		}
+    		catch (SQLException squeak)
+    		{
+    			// alarm doesn't exist yet for that day
+    			id = -1;
+    		}
+    		catch (CursorIndexOutOfBoundsException cioobe)
+    		{
+    			id = -1;
+    		}
+    		catch (IllegalStateException ise)
+    		{
+    			myDumbDB.open();
+    			addAlarm(v);
+    			return;
+    		}
+
+    		if (id >= 0) // then update alarm
+    		{
+    			myDumbDB.editAlarm(id, WEEK_DAYS[curr_day], raw_alarm);
+    		}
+    		else // create new alarm in database
+    		{
+    			myDumbDB.createAlarm(WEEK_DAYS[curr_day], raw_alarm);
+    		}
+
+    		Context context = getApplicationContext();
+    		Toast.makeText(context, "Alarm Set for " + WEEK_DAYS[curr_day], Toast.LENGTH_SHORT).show();
+    	}    	
+    	
     }
     
     public void addAlarm(View v)
@@ -234,6 +349,18 @@ public class AlarmsActivity extends Activity implements AdapterView.OnItemSelect
     	Context context = getApplicationContext();
 		Toast.makeText(context, "Removed Alarm for " + WEEK_DAYS[curr_day], Toast.LENGTH_SHORT).show();
     }
+
+    public void deleteDumbAlarm(View v)
+    {
+    	Spinner spinny = (Spinner) findViewById(R.id.day_spinner);
+    	int curr_day = spinny.getLastVisiblePosition();
+    	
+    	this.myDumbDB.deleteAlarm(WEEK_DAYS[curr_day]);
+    	clearAlarmFields();
+    	
+    	Context context = getApplicationContext();
+		Toast.makeText(context, "Removed Alarm for " + WEEK_DAYS[curr_day], Toast.LENGTH_SHORT).show();
+    }
     
     public void onItemSelected(AdapterView<?> parent, View v, int position,
 			long id)
@@ -258,5 +385,6 @@ public class AlarmsActivity extends Activity implements AdapterView.OnItemSelect
 	{
 		super.onStop();
 		myAlarmDB.close();
+		myDumbDB.close();
 	}
 }
